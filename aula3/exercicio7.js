f44376d8c3ca93b1cf04080432a864d3fd5fb0d1
Carla Janaina Braga <carla_agencia@hotmail.com>
2021-11-15 18:31:20 -0300

    exercicio 3 aula 3

---
+++ aula3/exercicio7.js
@@ -0,0 +1,49 @@
+var hojeVaiChover = true;
+
+if (hojeVaiChover) {
+  console.log("Leve seu guarda-chuva, hoje vai chover");
+} else {
+	console.log("Fique tranquilo, hoje será um dia de sol");
+}
+
+if (5 > 1) {
+    console.log("5 é maior que 1");
+  } else {
+      console.log("5 nao é maior que um");
+  }
+
+  if (2000000 > 3000000000) {
+    console.log("2000000 é maior que 3000000000");
+  } else {
+      console.log("2000000 nao é maior que 3000000000");
+  }
+
+  if ("Banana" == "Banana") {
+    console.log("tem o mesmo valor");
+  } else {
+      console.log("nao tem o mesmo valor");
+  }
+
+  if ("50" === 50) {
+    console.log("tem o mesmo valor e o mesmo tipo");
+  } else {
+      console.log("nao tem o mesmo valor ou o mesmo tipo");
+  }
+
+  if ("carro" != "bicicletas") {
+    console.log("valores diferentes ");
+  } else {
+      console.log("valores iguais");
+  }
+
+  if (50 > 27) {
+    console.log("50 e maior que 27 ");
+  } else {
+      console.log("50 nao e maior que 27");
+  }
+
+  if (975 <= 1200) {
+    console.log("975 e menor ou igual que 1200 ");
+  } else {
+      console.log("975 e maior ou igual a 1200");
+  }
